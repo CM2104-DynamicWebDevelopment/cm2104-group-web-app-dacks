6331f2fc481d616c702d6e8aab20735de75b56df
ArturMugin <47824482+ArturMugin@users.noreply.github.com>
2020-04-29 20:16:26 +0100

    Update popup.js

--- final/js/popup.js
+++ final/js/popup.js
@@ -1,9 +1,9 @@
-function myFunction() {
-  var x = document.getElementById("snackbar");
-  var p = document.getElementById('wash');
-  p.innerHTML += 'Remember to wash your hands!';
-  x.className = "show";
-  setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
-}
-
-window.onload=setTimeout(myFunction, 1000);
+$( document ).ready(function()
+{
+    setTimeout(function()
+    {
+        $("#wash").html("Remember to wash your hands")
+        $("#snackbar").show();
+        setTimeout(function(){ $("#snackbar").hide(); }, 3000);
+    }, 5000);    
+});
