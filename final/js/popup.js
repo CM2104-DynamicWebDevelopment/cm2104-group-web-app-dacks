452d9d439adfa1c136be6a72e8fc5163cab209ea
ArturMugin <47824482+ArturMugin@users.noreply.github.com>
2020-04-30 17:32:23 +0100

    Pop up message

--- final/js/popup.js
+++ final/js/popup.js
@@ -1,9 +1,20 @@
-$( document ).ready(function()
-{
-    setTimeout(function()
-    {
-        $("#wash").html("Remember to wash your hands")
-        $("#snackbar").show();
-        setTimeout(function(){ $("#snackbar").hide(); }, 3000);
-    }, 5000);    
+$(document).ready(function() {
+    setTimeout(function() {
+        $("#pop").text("Remember to wash your hands!");
+        $("#dialog").dialog({
+            autoOpen: false,
+            show: {
+                effect: "blind",
+                duration: 1000
+            },
+            hide: {
+                effect: "explode",
+                duration: 1000
+            }
+        });
+
+        $("#opener").ready(function() {
+            $("#dialog").dialog("open");
+        });
+    }, 5000);
 });
