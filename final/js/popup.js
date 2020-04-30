72648c5579b541ac2b0f295bd369b112dba0aa21
ArturMugin <47824482+ArturMugin@users.noreply.github.com>
2020-04-30 21:53:53 +0100

    add comments

--- final/js/popup.js
+++ final/js/popup.js
@@ -1,18 +1,23 @@
+//start fucntion
 $(document).ready(function() {
+//set time for function to appear after 5 econds
     setTimeout(function() {
+		//input the text inside the pop element
         $("#pop").text("Remember to wash your hands!");
         $("#dialog").dialog({
             autoOpen: false,
             show: {
+				//show the box with animation
                 effect: "blind",
                 duration: 1000
             },
+			//hide the box
             hide: {
                 effect: "explode",
                 duration: 1000
             }
         });
-
+		//function that close after 3 seconds
         $("#opener").ready(function() {
             $("#dialog").dialog("open");
 					setTimeout(function(){
@@ -22,3 +27,4 @@ $(document).ready(function() {
 
 }, 5000);
 });
+//end fucntion
